b9821818ffff6cddb108756abef97f9727129994
Amurius <artuslr78@gmail.com>
2024-10-24 11:03:23 +0200

    modifProfil

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,5 +6,6 @@ router.post('/ajoutuser', userController.postUser)
 router.get('/getVideastes', userController.getVideastes)
 router.get('/getEntreprises', userController.getEntreprises)
 router.get('/updateToAdmin', userController.updateToAdmin)
+router.post('/updateUser', userController.updateUser)
 
 module.exports = router;
